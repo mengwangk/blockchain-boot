9991408d0f4cf1b0d896a566bbc8840814d573b5
Koh Meng Wang <mengwangk@gmail.com>
2018-03-11 16:53:52 +0800

    Updated file

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -5,13 +5,16 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 500000,
+      gasPrice: 100000000
     },
     ourTestNet: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-      gas: 50000
+      gas: 100000,
+      gasPrice: 100
     }
   }
 };
